781c3d4e149941ece13ef69de1d4df7891ac5962
Yago Oliveira <yagodezoito@gmail.com>
2025-10-21 14:02:50 -0300

    Update Jogo da Forca.py

--- Jogo da Forca.py
+++ Jogo da Forca.py
@@ -7,18 +7,27 @@ palavraSorteada = random.choice(TemasSorteio.temas[TEMA])
 
 palavra = palavraSorteada
 letras_palavra = []
-vidas = 4
+letras_ditas = letras_palavra[:]
+vidas = 5
 vitoria = False
+
 from termcolor import colored
 from colorama import init
 
 init()
 
-def printCor(msg, cor):
-    print(colored(msg, cor))
+def printCor(msg, cor, Nacor=None):
+    '''
+    
+    :param msg: Aqui você pode escrever oque quer que apareça
+    :param cor: Agui você escolhe a cor das letras (deve ser em inglês e em letras minúsculas)
+    :param Nacor: aqui é o cur no fundo das letras, funciona igual a cor porém deve ter on_ antes da cor.
+    :return: 
+    '''
+    print(colored(msg, cor, Nacor))
 
 
-printCor(f'O TEMA É: {TEMA.upper()}', cor ='blue')
+printCor(f'O TEMA É: {TEMA.upper()}', cor ='blue', Nacor='on_black')
 
 while True:
     for letra in palavra:
@@ -27,14 +36,17 @@ while True:
         else:
             print('-', end = '')
     print()
-    tentativa = str(input(colored('Digite a sua letra: ', 'yellow'))).lower().strip()
+    tentativa = str(input(colored('Digite a sua letra: ', 'white'))).lower().strip()[0]
     letras_palavra.append(tentativa)
+    if tentativa not in letras_ditas:
+        letras_ditas.append(tentativa)
+    printCor(f'LETRAS DITAS: {", ".join(letras_ditas)}', 'blue', 'on_black')
     if tentativa not in palavra:
         vidas -= 1
-        printCor('ERROU!', 'red')
-    printCor('++' * 10, 'green')
-    printCor(f'Você tem {vidas} vidas', 'yellow')
-
+        printCor('ERROU!', 'red', 'on_black')
+        if vidas > 0:
+            printCor(f'Você tem {vidas} vidas', 'magenta', 'on_black')
+    printCor('++' * 10, 'blue')
     vitoria = True
     for letra in palavra:
         if letra not in letras_palavra:
@@ -45,6 +57,6 @@ while True:
         break
 
 if vitoria:
-    printCor(f'Parabéns você ganhou. A palavra era {palavra}', 'light_cyan')
+    printCor(f'parabéns você ganhou. A palavra era {palavra}', 'light_cyan')
 else:
-    printCor(f'Você perdeu. A palavra era {palavra}', 'light_magenta')
+    printCor(f'Você perdeu. A palavra era {palavra}', 'magenta')
